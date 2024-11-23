ac1bb8b72f5ea80450cd683d30f055695df52a56
Yuri Diego <contatoprofissional.ya@gmail.com>
2024-11-23 18:29:30 -0300

    Corrigido a implementacao de Matriz - Atividade 3

--- java/listas/src/br/com/unifacisa/binaryTrees/Main.java
+++ java/listas/src/br/com/unifacisa/binaryTrees/Main.java
@@ -8,11 +8,9 @@ public class Main {
 
 		int input = 20;
 		String result;
-		Binary tree = new Binary();
-		Matrix matrix = null; // Variável para armazenar a matriz gerada
+		Matrix matrix = new Matrix(101);
 
 		while (input != 0) {
-
 			result = showMenu();
 
 			if (result.equals("")) {
@@ -24,50 +22,40 @@ public class Main {
 				switch (input) {
 					case 1:
 						String item = JOptionPane.showInputDialog("Digite um numero para inserir na arvore!");
-						tree.insert(Integer.parseInt(item));
+						matrix.insert(Integer.parseInt(item));
 						break;
 					case 2:
 						String vl = JOptionPane.showInputDialog("Digite o valor que você deseja Remover!");
-						tree.remove(tree.getRoot(), Integer.parseInt(vl));
+						matrix.remove(Integer.parseInt(vl));
 						break;
 					case 3:
-						JOptionPane.showMessageDialog(null, "Os valores Serão exibidos no console!");
-						tree.showRoot();
-						System.out.println("");
+						JOptionPane.showMessageDialog(null, "A raiz da árvore será exibida no console!");
+						matrix.showRoot();
 						break;
 					case 4:
-						JOptionPane.showMessageDialog(null, "Os valores Serão exibidos no console!");
+						JOptionPane.showMessageDialog(null, "Os valores serão exibidos no console!");
 						System.out.println("Ordem simétrica:");
-						tree.inOrder(tree.getRoot());
-						System.out.println("");
+						matrix.inOrder(0);
+						System.out.println();
 						break;
 					case 5:
-						JOptionPane.showMessageDialog(null, "Os valores Serão exibidos no console!");
+						JOptionPane.showMessageDialog(null, "Os valores serão exibidos no console!");
 						System.out.println("Pré-ordem:");
-						tree.preOrder(tree.getRoot());
-						System.out.println("");
+						matrix.preOrder(0);
+						System.out.println();
 						break;
 					case 6:
-						JOptionPane.showMessageDialog(null, "Os valores Serão exibidos no console!");
+						JOptionPane.showMessageDialog(null, "Os valores serão exibidos no console!");
 						System.out.println("Pós-ordem:");
-						tree.postOrder(tree.getRoot());
-						System.out.println("");
+						matrix.postOrder(0);
+						System.out.println();
 						break;
-					case 7: // Converte a árvore para matriz
-						if (tree.getRoot() == null) {
-							JOptionPane.showMessageDialog(null, "A arvore esta vazia!");
-						} else {
-							JOptionPane.showMessageDialog(null, "A arvore foi convertida para uma matriz!");
-							matrix = new Matrix(tree); // Converte a árvore em matriz
-						}
+					case 7:
+						JOptionPane.showMessageDialog(null, "Os valores Serão exibidos no console!");
+						matrix.showTree();
 						break;
-					case 8: // Exibe a matriz
-						if (matrix == null) {
-							JOptionPane.showMessageDialog(null, "Matriz ainda nao gerada, escolha a opcao 7 primeiro.");
-						} else {
-							JOptionPane.showMessageDialog(null, "A matriz sera exibida no console!");
-							matrix.printMatrixWithIndex(); // Exibe a matriz com índices
-						}
+					case 0:
+						JOptionPane.showMessageDialog(null, "Saindo...");
 						break;
 				}
 			}
@@ -83,8 +71,7 @@ public class Main {
 				"\n 4: Para Exibir ordem simétrica " +
 				"\n 5: Para Exibir pré-ordem " +
 				"\n 6: Para Exibir pós-ordem " +
-				"\n 7: Converter a árvore para matriz" +
-				"\n 8: Exibir a matriz gerada" +
+				"\n 7: Para Exibir a árvore como matriz " +
 				"\n 0: Para Sair");
 
 		if (result == null) {
--- java/listas/src/br/com/unifacisa/binaryTrees/Matrix.java
+++ java/listas/src/br/com/unifacisa/binaryTrees/Matrix.java
@@ -1,64 +1,147 @@
 package br.com.unifacisa.binaryTrees;
 
 public class Matrix {
-
-    private int[][] matrix;
+    private Node[] tree;
     private int size;
+    private final int max_Size;
 
-    public Matrix(Binary binaryTree) {
-        this.matrix = new int[10][3];   // inicializa com 10 linhas e 3 colunas
+    public Matrix(int max_Size) {
+        this.tree = new Node[max_Size];
+        this.max_Size = max_Size;
         this.size = 0;
-        buildMatrix(binaryTree.getRoot());
     }
 
-    private void buildMatrix(Node node) {
-        buildMatrix(node, 0); // Starta da raiz com índice 0
-    }
-
-    private int buildMatrix(Node node, int currentIndex) {
-        if (node == null) {
-            return -1; // Indice inválido para nós nulos
+    public void insert(int value) {
+        if (size == 0) {
+            tree[0] = new Node(value); // Raiz na posição 0
+            System.out.println("Inserindo " + value + " na raiz.");
+            size++;
+        } else {
+            insert(0, value);
         }
-
-        // Adiciona o nó atual na matriz
-        addNode(node.getValue(), -1, -1);
-
-        // Salva o indice atual
-        int leftIndex = buildMatrix(node.getLeft(), size); // Indice do filho esquerdo
-        int rightIndex = buildMatrix(node.getRight(), size); // Indice do filho direito
-
-        // Atualiza os indices esquerdo e direito do nó atual
-        matrix[currentIndex][1] = leftIndex;
-        matrix[currentIndex][2] = rightIndex;
-
-        return currentIndex;
     }
 
+    private void insert(int index, int value) {
+        if (tree[index] == null) return;
 
-    private void addNode(int value, int leftIndex, int rightIndex) {
-        if (size == matrix.length) {
-            resizeMatrix();
+        if (value < tree[index].getValue()) {
+            int leftIndex = 2 * index + 1;
+            if (tree[leftIndex] == null) {
+                tree[leftIndex] = new Node(value);
+                System.out.println("Inserindo " + value + " a esquerda de " + tree[index].getValue());
+                size++;
+            } else {
+                insert(leftIndex, value);
+            }
+        } else if (value > tree[index].getValue()) {
+            int rightIndex = 2 * index + 2;
+            if (tree[rightIndex] == null) {
+                tree[rightIndex] = new Node(value);
+                System.out.println("Inserindo " + value + " a direita de " + tree[index].getValue());
+                size++;
+            } else {
+                insert(rightIndex, value);
+            }
         }
-        matrix[size][0] = value;       // Valor do nó
-        matrix[size][1] = leftIndex;  // Índice do filho esquerdo
-        matrix[size][2] = rightIndex; // Índice do filho direito
-        size++;
     }
 
-    private void resizeMatrix() {
-        int[][] newMatrix = new int[matrix.length * 2][3];
-        System.arraycopy(matrix, 0, newMatrix, 0, matrix.length);
-        matrix = newMatrix;
-    }
-
-    public void printMatrixWithIndex() {
-        System.out.println("ind\tVal\tLef\tRig");
-        for (int i = 0; i < size; i++) {
-            System.out.printf("%d\t%d\t%d\t%d\n",
-                    i,                // Índice do nó
-                    matrix[i][0],     // Valor do nó
-                    matrix[i][1],     // Índice do filho esquerdo
-                    matrix[i][2]);    // Índice do filho direito
+    public void remove(int value) {
+        int index = find(0, value);
+        if (index == -1) {
+            System.out.println("Valor não encontrado na árvore.");
+            return;
         }
+        removeAt(index);
+    }
+
+    private int find(int index, int value) {
+        if (index >= max_Size || tree[index] == null) {
+            return -1;
+        }
+        if (tree[index].getValue() == value) {
+            return index;
+        }
+        int leftResult = find(2 * index + 1, value);
+        return leftResult != -1 ? leftResult : find(2 * index + 2, value);
+    }
+
+    private void removeAt(int index) {
+        if (index >= max_Size || tree[index] == null) return;
+
+        int leftIndex = 2 * index + 1;
+        int rightIndex = 2 * index + 2;
+
+        if (tree[leftIndex] == null && tree[rightIndex] == null) {
+            // Caso 1: Nó folha
+            tree[index] = null;
+        } else if (tree[leftIndex] != null && tree[rightIndex] == null) {
+            // Caso 2: Apenas filho esquerdo
+            tree[index] = tree[leftIndex];
+            tree[leftIndex] = null;
+        } else if (tree[rightIndex] != null && tree[leftIndex] == null) {
+            // Caso 2: Apenas filho direito
+            tree[index] = tree[rightIndex];
+            tree[rightIndex] = null;
+        } else {
+            // Caso 3: Dois filhos - substitui pelo sucessor
+            int successorIndex = findMin(rightIndex);
+            tree[index].setValue(tree[successorIndex].getValue());
+            removeAt(successorIndex);
+        }
+        size--;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void inOrder(int index) {
+        if (index >= max_Size || tree[index] == null) return;
+
+        inOrder(2 * index + 1);
+        System.out.print(tree[index].getValue() + " ");
+        inOrder(2 * index + 2);
+    }
+
+    public void preOrder(int index) {
+        if (index >= max_Size || tree[index] == null) return;
+
+        System.out.print(tree[index].getValue() + " ");
+        preOrder(2 * index + 1);
+        preOrder(2 * index + 2);
+    }
+
+    public void postOrder(int index) {
+        if (index >= max_Size || tree[index] == null) return;
+
+        postOrder(2 * index + 1);
+        postOrder(2 * index + 2);
+        System.out.print(tree[index].getValue() + " ");
+    }
+
+    public void showTree() {
+        System.out.println("Ind\tVal\tLef\tRig");
+        for (int i = 0; i < max_Size; i++) {
+            if (tree[i] != null) {
+                int leftIndex = 2 * i + 1 < max_Size && tree[2 * i + 1] != null ? 2 * i + 1 : -1;
+                int rightIndex = 2 * i + 2 < max_Size && tree[2 * i + 2] != null ? 2 * i + 2 : -1;
+                System.out.println(i + "\t" + tree[i].getValue() + "\t" + leftIndex + "\t" + rightIndex);
+            }
+        }
+    }
+
+    public void showRoot() {
+        if (isEmpty()) {
+            System.out.println("A árvore está vazia!");
+        } else {
+            System.out.println("Raiz: " + tree[0].getValue());
+        }
+    }
+
+    private int findMin(int index) {
+        while (2 * index + 1 < max_Size && tree[2 * index + 1] != null) {
+            index = 2 * index + 1;
+        }
+        return index;
     }
 }
